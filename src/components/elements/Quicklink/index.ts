802159d842a374845b9d87cce5b9c6382fc13ebc
h.mchoi@kalmate.net <h.mchoi@kalmate.net>
2025-10-27 19:42:01 +0900

    #18 : quicklink 컴퍼넌트 추가

---
+++ src/components/elements/Quicklink/index.ts
@@ -0,0 +1,5 @@
+export { default } from './Quicklink';
+export type { QuicklinkProps } from './Quicklink';
+
+export { default as QuicklinkGroup } from './QuicklinkGroup';
+export type { QuicklinkGroupProps, QuicklinkItem } from './QuicklinkGroup';
